46f1cbcb5c2e71ca53cdbfa791dce88a64b778fa
Dagan Herceg <dagan.herceg@gmail.com>
2017-10-26 23:45:39 +1100

    Added url link to SignUps. for some reason it wasn't there???

--- src/beacon_landing/urls.py
+++ src/beacon_landing/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.models import User
+from signups.models import SignUp
 from rest_framework import routers, serializers, viewsets
 
 class SignUpSerializer(serializers.HyperlinkedModelSerializer):
@@ -37,9 +38,9 @@ urlpatterns = [
   url(r'^$', 'signups.views.home', name='home'),
 	url(r'^thank-you', 'signups.views.thankyou', name='thankyou'),
 	url(r'^about-us', 'signups.views.aboutus', name='aboutus'),
-    url(r'^profile', 'signups.views.profile', name='profile'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+  url(r'^profile', 'signups.views.profile', name='profile'),
+  url(r'^admin/', admin.site.urls),
+  url(r'^accounts/', include('registration.backends.default.urls')),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
