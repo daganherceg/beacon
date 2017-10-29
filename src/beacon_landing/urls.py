c6a5d7ba067a8d6577a3ebc4e60867dea9ad28d7
Eve Martin-Jones <evemartinjones95@gmail.com>
2017-10-29 20:43:51 +1100

    add rest api for creating a new user

--- src/beacon_landing/urls.py
+++ src/beacon_landing/urls.py
@@ -20,22 +20,24 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
-class SignUpSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
-    model = SignUp
-    fields = ('first_name', 'last_name', 'email', 'timestamp', 'updated')
+    model = User
+    fields = ('url', 'username', 'email', 'is_staff')
 
-class SignUpsViewSet(viewsets.ModelViewSet):
-  queryset = SignUp.objects.all()
-  serializer_class = SignUpSerializer
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
 
+# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'signups', SignUpsViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-	url(r'^', include(router.urls)),
+  url(r'^', include(router.urls)),
   url(r'^$', 'signups.views.home', name='home'),
-	url(r'^thank-you', 'signups.views.thankyou', name='thankyou'),
+  url(r'^thank-you', 'signups.views.thankyou', name='thankyou'),
+  url(r'^users/register', 'signups.views.register_user'),
 	url(r'^about-us', 'signups.views.aboutus', name='aboutus'),
     url(r'^profile', 'signups.views.profile', name='profile'),
     url(r'^admin/', admin.site.urls),
--- src/signups/views.py
+++ src/signups/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from beacon_landing.urls import UserSerializer
 
 from .models import SignUp
 from .forms import SignUpForm
@@ -13,15 +19,15 @@ def home(request):
 		save_it.save()
 		messages.success(request, 'Thank you for joining')
 		return HttpResponseRedirect('/thank-you')
-	return render_to_response("signup.html", 
-								locals(), 
+	return render_to_response("signup.html",
+								locals(),
 								context_instance=RequestContext(request))
 
 
 def thankyou(request):
 	form = SignUpForm(request.POST or None) # sends to server
-	return render_to_response("thankyou.html", 
-							locals(), 
+	return render_to_response("thankyou.html",
+							locals(),
 							context_instance=RequestContext(request))
 
 def aboutus(request):
@@ -36,3 +42,12 @@ def profile(request):
 			"queryset": SignUp.objects.all()
 		}
 	return render(request, "profile.html", context)
+
+@api_view(['POST'])
+def register_user(request):
+  s = UserSerializer(data=request.DATA, context={'request': request})
+  if s.is_valid():
+    User.objects.create_user(s.init_data['email'], s.init_data['username'], s.init_data['password'])
+    return Response(s.data, status=status.HTTP_201_CREATED)
+  else:
+    return Response(s._errors, status=status.HTTP_400_BAD_REQUEST)
