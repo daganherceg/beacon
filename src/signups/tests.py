1aa20c10eee9e24f350c14fb908e90b450c6c1cf
Dagan Herceg <dagan.herceg@gmail.com>
2017-11-02 23:31:23 +1100

    Initial working test for models

--- src/signups/tests.py
+++ src/signups/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from models import UserProfile
+
+"""
+TESTING of the models class will create temporary, clean version of 
+database which can be manipulated without consequence
+"""
+
+""" TEST user creation """
+
+""" TEST multiple users """
+
+""" TEST profile creation """
+
+""" TEST beacon creation """
+
+""" TEST Beacpn linking """
+
+class UserTestCase(TestCase):
+	def setUp(self):
+		User.objects.create(username="test1", email="test1@test1.com")
+		User.objects.create(username="test2", email="test2@test2.com")
+
+	def test_user_setup(self):
+		e1 = User.objects.get(username="test1")
+		self.assertEqual(e1.email, "test1@test1.com")
